d4e2b42e4efc53ce59161d397a9fe821a674a7ce
Sara <123300249+sarav929@users.noreply.github.com>
2024-10-17 11:15:06 +0100

    add base styling and logo img

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import createNav from './nav'
 
 createNav()
--- src/nav.js
+++ src/nav.js
@@ -1,20 +1,29 @@
+import './styles/base.css'
+import logo from './img/logo.png'
+
 const createNav = () => {
 
     const nav = document.querySelector('nav')
     
     const logoDiv = document.createElement('div')
     logoDiv.setAttribute('id', 'logo')
-    logoDiv.innerHTML = 'LOGO'
+    const logoImg = document.createElement('img')
+    logoImg.setAttribute('id', 'kiyomi-logo')
+    logoImg.src = logo
+    logoDiv.appendChild(logoImg)
 
     const navBtns = document.createElement('div')
     navBtns.setAttribute('id', 'nav-btns')
 
     const homeBtn = document.createElement('button')
     homeBtn.setAttribute('id', 'home-btn')
+    homeBtn.innerText = 'home'
     const menuBtn = document.createElement('button')
     menuBtn.setAttribute('id', 'menu-btn')
+    menuBtn.innerText = 'menu'
     const contactsBtn = document.createElement('button')
     contactsBtn.setAttribute('id', 'contacts-btn')
+    contactsBtn.innerText = 'contacts'
 
     navBtns.appendChild(homeBtn)
     navBtns.appendChild(menuBtn)
@@ -22,7 +31,6 @@ const createNav = () => {
 
     nav.appendChild(logoDiv)
     nav.appendChild(navBtns)
-
 }
 
 export default createNav
