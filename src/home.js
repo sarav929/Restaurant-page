6e86488765cfc2bb0c43eb55071da856099fa576
Sara <123300249+sarav929@users.noreply.github.com>
2024-10-17 12:15:54 +0100

    add content to about section

--- src/home.js
+++ src/home.js
@@ -1,6 +1,10 @@
 import './styles/base.css'
 import './styles/home.css'
 
+import barista from './img/barista.jpg'
+
+ 
+
 function createHeader() {
     const header = document.createElement('header')
     const body = document.querySelector('body')
@@ -28,9 +32,58 @@ function createHeader() {
     body.appendChild(header)
 }
 
+function createAbout() {
+    const body = document.querySelector('body')
+    const main = document.createElement('main') 
+
+    body.appendChild(main)    
+
+    const aboutDiv = document.createElement('div')
+    aboutDiv.setAttribute('class', 'about')
+
+    main.appendChild(aboutDiv)
+
+    const aboutText = document.createElement('div')
+    aboutText.setAttribute('class', 'about-text')
+
+    aboutDiv.appendChild(aboutText)
+
+    const title = document.createElement('span')
+    title.setAttribute('class', 'about-title')
+
+    const h1one = document.createElement('h1')
+    h1one.setAttribute('class', 'about-us')
+    h1one.innerText = 'About Us'
+
+    const h1two = document.createElement('h1')
+    h1two.setAttribute('class', 'jap')
+    h1two.innerText = '• 清美'
+
+    title.appendChild(h1one)
+    title.appendChild(h1two)
+
+    aboutText.appendChild(title)
+
+    const p1 = document.createElement('p')
+    const p2 = document.createElement('p')
+    const p3 = document.createElement('p')
+
+    p1.innerHTML = "At Kiyomi Café, we blend the elegance of Japanese tradition with contemporary café culture."
+    p2.innerHTML = "Our mission is to create a space where every cup of coffee, tea, and each bite of our freshly baked goods tells a story—infused with authentic Japanese flavours and crafted with modern techniques.<br> From our signature coffee blends to our unique pastries, we’re dedicated to delivering an experience that nourishes both the body and soul."
+    p3.innerHTML = "Whether you're here to relax, connect, or explore new tastes, Kiyomi Café is where tradition and innovation meet."
+    aboutText.appendChild(p1)
+    aboutText.appendChild(p2)
+    aboutText.appendChild(p3)
+
+    const img = document.createElement('img')
+    img.setAttribute('class', 'about-img')
+    img.src = barista
+    aboutDiv.appendChild(img)
+}
+
 const createHomePage = () => {
     createHeader()
-    const main = document.querySelector('main')    
+    createAbout()   
 }
 
 export default createHomePage
